939c4397db8e738374a26c188f548ee5bc1e0104
mm jxx <r00txxyyzz@gmail.com>
2021-09-17 00:26:41 +0800

    benchmark coding

--- benches/client_serv.rs
+++ benches/client_serv.rs
@@ -1,14 +1,18 @@
 #![feature(test)]
 extern crate test;
 
-use std::io;
+use std::{io, time};
 use std::net::SocketAddr;
 use tokio::net::TcpStream;
-use cakerabbit_core::{Client, Value};
+use cakerabbit_core::{Client, Value, Register, new_selector, SelectorTyp, CakeClient, CakeError, FailMode, Failtry};
 use tokio_util::compat::TokioAsyncReadCompatExt;
+use std::future::Future;
+use futures::TryFutureExt;
+use std::thread;
 
 #[tokio::main]
 async fn client_to_serv() -> io::Result<()> {
+  /*
   let addr: SocketAddr = "127.0.0.1:9527".parse().unwrap();
   let socket = TcpStream::connect(&addr).await?;
   let client = Client::new(socket.compat());
@@ -18,6 +22,27 @@ async fn client_to_serv() -> io::Result<()> {
   match client.request("say_hello", &["foo".into()]).await {
     Ok(response) => println!("Response: {:?}", response),
     Err(e) => println!("Error: {:?}", e),
+  }
+
+  Ok(())
+   */
+
+  let mut cake_client = CakeClient::new("pomid/".into(),
+                                        "EchoRs".into(),
+                                        "consul".into(),
+                                        "consul_test:8500".into(),
+                                        SelectorTyp::RoundRobin,
+                                        FailMode::FailFast);
+  let res = cake_client.call("say_hello",
+                             &["foo".into()]).await;
+  match res {
+    Ok(rsp) => {
+      println!("rsp ------------ {}", rsp)
+    }
+
+    Err(err) => {
+      println!("err ------------------ {:?}", err)
+    }
   }
 
   Ok(())
@@ -32,7 +57,7 @@ mod tests {
   #[bench]
   fn bench_client_to_serv(b: &mut Bencher) {
     // b.iter(|| println!("bench..."));
-    b.iter(|| client_to_serv() );
+    b.iter(|| client_to_serv());
   }
 }
 
